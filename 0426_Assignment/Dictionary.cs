3074d3d667e270391a283b6bdfe44a05e45b1824
Jung BoBae <acarid27@gmail.com>
2023-04-26 21:24:01 +0900

    feat: Add Dictionary Class and TestCodes
    딕셔너리 클래스 추가 및 테스트코드 작성
    인덱서, 추가, 찾기, 삭제 구현

--- 0426_Assignment/Dictionary.cs
+++ 0426_Assignment/Dictionary.cs
@@ -4,9 +4,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _0426_Assignment
+namespace DataStructure
 {
-    internal class Dictionary
+    internal class Dictionary<TKey, TValue> where TKey : IEquatable<TKey>
     {
+        private const int DefaultCapacity = 1000;
+        private Entry[] table;
+        public Dictionary() 
+        { 
+            table = new Entry[DefaultCapacity]; // Dictionary가 생성되면 1000개의 엔트리 배열 생성
+        }
+        private struct Entry
+        {
+            internal TKey key;
+            internal TValue value;
+
+            internal enum EntryState { None, Using, Deleted}
+            internal EntryState state;
+        }
+        /// <summary>
+        /// get: 키에 해당하는 값이 있다면 값 반환 없다면 에러
+        /// set: 키에 해당하는 값이 있다면 값 변경 없다면 에러
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public TValue this[TKey key]
+        {
+            get 
+            {
+                TValue value;
+                if(TryGetValue(key,out value))
+                {
+                    return value;
+                }
+                else{ throw new InvalidOperationException(); } // 없는 값 접근 에러
+            }
+            set
+            {
+                int index = FindIndex(key);
+                if(index == -1) { throw new InvalidOperationException(); }
+                table[index].value = value;
+            }
+        }
+        public void Add(TKey key, TValue value)
+        {
+            int index = FindIndex(key);
+            if (index != -1) throw new InvalidOperationException(); // 중복된 키라면 에러
+
+            index = Math.Abs(key.GetHashCode()) % table.Length;
+            while (table[index].state.Equals(Entry.EntryState.Using)) // 쓰고있는 칸이 아닐 때까지 반복
+            {
+                index = (index * index) % table.Length;
+            }
+            table[index].key = key;
+            table[index].value = value;
+            table[index].state = Entry.EntryState.Using;
+        }
+        /// <summary>
+        /// 일치하는 키가 있다면 out에 value 저장후 true 리턴 없다면 value에 default 저장 후 false 반환
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetValue(TKey key, out TValue value) 
+        {
+            int index = FindIndex(key);
+            if(index == -1)
+            {
+                value = default;
+                return false;
+            }
+            value = table[index].value;
+            return true;
+        }
+        public bool Remove(TKey key)
+        {
+            int index = FindIndex(key);
+            if(index == -1) return false; // 없는 값 제거 시 실패 리턴
+            table[index].state = Entry.EntryState.Deleted;
+            return true;
+        }
+        /// <summary>
+        /// 일치하는 key가 있다면 index 반환 없다면 -1 반환
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private int FindIndex(TKey key)
+        {
+            int index = Math.Abs(key.GetHashCode()) % table.Length;
+            while (table[index].state != Entry.EntryState.None) // None이 아닌동안 탐색
+            {
+                if (key.Equals(table[index].key))
+                {
+                    // 만약 찾은 키가 Deleted 키 일 경우 -1 리턴
+                    if (table[index].state == Entry.EntryState.Deleted) return -1;
+                    return index;
+                }
+                index = (index * index) % table.Length;
+            }
+            return -1;
+        }
+
     }
 }
--- 0426_Assignment/Program.cs
+++ 0426_Assignment/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataStructure.Dictionary<string, int> dictionary = new DataStructure.Dictionary<string, int>();
+            dictionary.Add("a", 1);
+            dictionary.Add("b", 2);
+            dictionary.Add("c", 3);
+            dictionary.Add("d", 4);
+
+            if(dictionary.TryGetValue("a", out int value)) { Console.WriteLine(value); }
+            dictionary["a"] = 0;
+            Console.WriteLine(dictionary["a"]);
+
+            //dictionary.Add("b", 3); // 중복 값 삽입
+            //dictionary["e"] = 5; // 없는 값 set
+            //Console.WriteLine(dictionary["e"]); // 없는 값 접근
+            dictionary.Remove("a");
+            //Console.WriteLine(dictionary["a"]); // 삭제한 값 접근
         }
     }
 }
